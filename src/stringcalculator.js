bbca0edbe299216ddd59b767a5bd54436f5152fe
Mohanish Mankar <mohanish.mankar@lovebonito.com>
2021-08-02 13:18:58 +0530

    assessment initial commit

--- src/stringcalculator.js
+++ src/stringcalculator.js
@@ -1,3 +1,53 @@
+const constructError = (type, argument) => {
+  switch (type) {
+    case 'negativeNumbers':
+      let errorMessage = 'negatives not allowed: '
+
+      argument.forEach(number => {
+        errorMessage += `${number},`
+      })
+
+      // remove trailing comma
+      errorMessage = errorMessage.slice(0, -1)
+
+      return errorMessage
+  }
+}
+
+const getCustomDelimitter = (numbers) => {
+  let customDelimitter = ''
+
+  if (numbers.substring(0, 2) === '//') {
+    customDelimitter = numbers[2]
+  }
+
+  return customDelimitter
+}
+
 module.exports = (numbers) => {
-    return 0;
+  let customDelimitter = getCustomDelimitter(numbers)
+  let splittedNumbers = []
+  let result = 0
+  let negativeNumbers = []
+
+  if (customDelimitter !== '') {
+    numbers = numbers.slice(2)
+    splittedNumbers = numbers.split('\n').join(',').split(customDelimitter).join(',').split(',')
+  } else {
+    splittedNumbers = numbers.split('\n').join(',').split(',')
+  }
+
+  splittedNumbers.forEach(number => {
+    const numberInteger = Number(number)
+
+    if (numberInteger < 0) { negativeNumbers.push(numberInteger) } else if (numberInteger >= 1000) return
+
+    result += numberInteger
+  })
+
+  if (negativeNumbers.length > 0) {
+    throw new Error(constructError('negativeNumbers', negativeNumbers))
+  }
+
+  return result
 }
--- src/stringcalculator.test.js
+++ src/stringcalculator.test.js
@@ -3,9 +3,76 @@ const add = require('./stringcalculator')
 
 describe('add()', () => {
   test('Given empty string, should returns 0', () => {
-    const given = ""
+    const given = ''
 
-    const expected = 0;
+    const expected = 0
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+  test('Given a single number, it should return that number', () => {
+    const given = '20'
+
+    const expected = 20
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+
+  test('Given two numbers, it should return that number', () => {
+    const given = '20,15'
+
+    const expected = 35
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+  test('Given a list of numbers, it should return their sum', () => {
+    const given = '20,10,13,6'
+
+    const expected = 49
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+  test('Given a list of numbers, it should throw error for negative numbers', () => {
+    try {
+      const given = '-20,10,-13,6'
+
+      add(given)
+
+      // Fail test if above expression doesn't throw anything.
+      expect(true).toBe(false)
+    } catch (e) {
+      expect(e.message).toBe('negatives not allowed: -20,-13')
+    }
+  })
+  test('Given a list of numbers, it should ignore the numbers greater than 1000 and return the sum of remaining', () => {
+    const given = '20,10,5000,1001,43121'
+
+    const expected = 30
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+  test('Given a list of numbers, it should support newline delimiter', () => {
+    const given = '1\n2,3\n10,15'
+
+    const expected = 31
+
+    const actual = add(given)
+
+    expect(actual).toEqual(expected)
+  })
+  test('Given a list of numbers with custom delimitter, it should return the sum', () => {
+    const given = '//@\n1@2'
+
+    const expected = 3
 
     const actual = add(given)
 
